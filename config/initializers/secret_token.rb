2fa2bcb7d60801f83a98dd83e0fa140f437921ba
Chien Kuo <chien.cc.kuo@gmail.com>
2013-10-30 21:51:15 -0400

    init rails.'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideacamp::Application.config.secret_token = '04039fe9bc400a849f4fa5f83625ea148c5e7787e86cab77b9b961e2e34d5ecb214032768f986c33804c9b2f85991039441853d43a49f902fcbb1137b70d328b'
