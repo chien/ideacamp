b14eea96ee8aa0c58ccec243a24f7e2e7d95021a
Chien Kuo <chien.cc.kuo@gmail.com>
2013-10-31 01:14:43 -0400

    adds more feed.

--- app/commands/rss_feed_fetcher.rb
+++ app/commands/rss_feed_fetcher.rb
@@ -7,17 +7,21 @@ class RSSFeedFetcher
     feeds = Feedzirra::Feed.fetch_and_parse(feed_urls)
 
     feeds.each do |url, feed_burner|
-      feed_burner.entries.each do |entry|
-        article = Article.find_or_initialize_by(url: entry.url)
-        article.attributes = {
-          title: entry.title,
-          content: entry.content,
-          url: entry.url,
-          author: entry.author,
-          summary: entry.summary,
-          published_at: entry.published
-        }
-        article.save!
+      begin
+        feed_burner.entries.each do |entry|
+          article = Article.find_or_initialize_by(url: entry.url)
+          article.attributes = {
+            title: entry.title,
+            content: entry.content,
+            url: entry.url,
+            author: entry.author,
+            summary: entry.summary,
+            published_at: entry.published
+          }
+          article.save!
+        end
+      rescue Exception => e
+        puts "#{url} fetch failed!"
       end
     end
   end
--- config/initializers/fetch_feeds.rb
+++ config/initializers/fetch_feeds.rb
@@ -1,18 +1,88 @@
 feed_urls = ["http://feeds.feedburner.com/AVcVentureCapitalAndTechnology",
              "http://feeds.feedburner.com/guykawasaki/Gypm",
-"http://feeds.feedburner.com/FeldThoughts",
-"http://feeds.feedburner.com/BothSidesOfTheTable",
-"http://feeds.feedburner.com/redeyevc",
-"http://lsvp.com/feed",
-"http://www.trueventures.com/feed",
-"http://larrycheng.com/feed"
+             "http://feeds.feedburner.com/FeldThoughts",
+             "http://feeds.feedburner.com/BothSidesOfTheTable",
+             "http://feeds.feedburner.com/redeyevc",
+             "http://feeds.feedburner.com/thisisgoingtobebig",
+             "http://cdixon.org/feed",
+             "http://feeds.feedburner.com/venturecast",
+             "http://feeds.feedburner.com/InfectiousGreed",
+             "http://feeds.feedburner.com/Xconomy_Full",
+             "http://feeds.feedburner.com/VCevents",
+             "http://feeds.feedburner.com/AskTheVC",
+             "http://feeds.feedburner.com/beyondvc",
+             "http://genuinevc.com/archives/rss.xml",
+             "http://feeds.feedburner.com/BabblingVC",
+             "http://feeds.feedburner.com/typepad/nqcX",
+             "http://feeds.feedburner.com/typepad/FredDestin",
+             "http://feeds.feedburner.com/Rachid_Sefrioui_On_Venture_Capital",
+             "http://blog.pmarca.com/feed",
+             "http://www.navigatingventure.com/feed",
+             "http://thewholestack.com/feed",
+             "http://feeds2.feedburner.com/vcu",
+             "http://feeds.feedburner.com/VentureCapitalCafe",
+             "http://lsvp.com/feed",
+             "http://www.venturewoods.org/feed",
+             "http://feeds.feedburner.com/VC_Adventure",
+             "http://www.trueventures.com/feed",
+             "http://larrycheng.com/feed",
+             "http://feeds.feedburner.com/thenextweb",
+             "http://feeds.feedburner.com/Techcrunch",
+             "http://feeds.feedburner.com/venturebeat",
+             "http://feeds.feedburner.com/WorkHappy",
+             "http://feeds.feedburner.com/StartupMeme",
+             "http://feeds.feedburner.com/onstartups",
+             "http://feeds.feedburner.com/Trizle",
+             "http://mpd.me/feed",
+             "http://feeds.feedburner.com/vcdave",
+             "http://allensblog.typepad.com/allensblog/atom.xml",
+             "http://an.ton.io/blog/xml/rss",
+             "http://feeds.feedburner.com/forentrepreneurs/",
+             "http://www.pascalsview.com/feed/atom",
+             "http://www.jumpstartinc.org/blog.aspx?RSS=1",
+             "http://feeds.feedburner.com/christine",
+             "http://www.julesmaltz.com/rss",
+             "http://processanditeration.tumblr.com/rss",
+             "http://feeds2.feedburner.com/Highway12Ventures",
+             "http://thebij.com/feed",
+             "http://feeds.feedburner.com/ataussig",
+             "http://feeds.feedburner.com/VcInDc",
+             "http://feeds.feedburner.com/13HoursToThink",
+             "http://feeds.feedburner.com/Mcinblog",
+             "http://feeds.feedburner.com/jasonmendelson",
+             "http://feeds.feedburner.com/navfund",
+             "http://www.agilevc.com/blog/rss.xml",
+             "http://feeds.feedburner.com/venturingforth",
+             "http://hazard.typepad.com/hazard-lights/atom.xml",
+             "http://vcmike.wordpress.com/feed",
+             "http://feeds.feedburner.com/theFrankPetersShow",
+             "http://feeds.feedburner.com/blogs/sSCW",
+             "http://feeds.feedburner.com/EricFriedman",
+             "http://feeds.feedburner.com/VentureExplorer",
+             "http://www.vcaonline.com/news/rss/index.asp"
+
 ]
 
+# http://feeds.feedburner.com/VC_Adventure fetch failed!
+# http://feeds2.feedburner.com/TheMogulMomBlog fetch failed!
+# http://www.scalevp.com/feed fetch failed!
+# http://feeds.feedburner.com/startupnation/svep fetch failed!
+# http://ontheflyingbridge.wordpress.com/
+#http://www.bitsofcents.com/
+#http://ryanspoon.com/blog/
+#http://www.nea.com/blog
+# http://jasonball.com/2007/05/17/techbytes-en-espaol/
+# http://www.purevc.com/pure_vc/
+# http://www.vcaonline.com/
+# http://www.redherring.com/Home/
+# http://www.usv.com/
+# http://jeffnolan.com/wp/
+# https://vcexperts.com
 # www.venturecompany.com
 # www.venturemedia.nl
 # http://abovethecrowd.com/
 # http://500hats.typepad.com/
 # http://bijansabet.com/
-# 
+# http://firstround.com/blog
 
 RSSFeedFetcher.new(feed_urls).execute
